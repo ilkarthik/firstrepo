98272707d37f88d275495bd84eb4a4d44babd529
ilkarthik <124597490+ilkarthik@users.noreply.github.com>
2023-03-13 12:12:06 +0530

    Update file.java

--- file.java
+++ file.java
@@ -2,8 +2,6 @@ public class file
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+System.out.println("Hello World");  
 }  
 }  
